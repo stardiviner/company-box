dfa4246ed2db36cb3c3b905ad7110ce0973bf95b
tam5 <arimiller92@gmail.com>
2018-11-01 12:56:30 -0400

    initially make undecorated false

--- company-box.el
+++ company-box.el
@@ -197,7 +197,7 @@ Examples:
     (tool-bar-lines . 0)
     (line-spacing . 0)
     ;; (unsplittable . nil)
-    (undecorated . t)
+    (undecorated . nil)
     (top . -1)
     (visibility . nil)
     (mouse-wheel-frame . nil)
